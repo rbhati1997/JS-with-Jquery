d16591aefd76f23fa37d40c4d8e715523b787580
Your Name <you@example.com>
2020-03-06 13:36:00 +0530

    Make validation function for form fields.

--- js/my.js
+++ js/my.js
@@ -9,16 +9,21 @@ $(document).ready(function(){
     var age = $("#age").val();
     var rowCount = $('#myTable tr').length + 1;
 
-    $('#myTable').append(
-            '<tr>'
-            +'<td id="order">'+rowCount+'</td>'
-            +'<td>'+name+'</td>'
-            +'<td>'+age+'</td>'
-            +'<td><button id="view_btn" class="btn btn-default" data-toggle="modal" data-target="#exampleModal">view</button><button id="update_btn" class="btn btn-default" data-toggle="modal" data-target="#exampleModal">update</button><button id="delete_btn" class="btn btn-default">delete</button><button id="down_btn" class="btn btn-default">down</button><button id="up_btn" class="btn btn-default">up</button></td>'
-            +'</tr>');
+    var valid = validateData(name, age);
 
-    $('#name').val("")
-    $('#age').val("")
+    if(valid) {
+        $('#myTable').append(
+                '<tr>'
+                +'<td id="order">'+rowCount+'</td>'
+                +'<td>'+name+'</td>'
+                +'<td>'+age+'</td>'
+                +'<td><button id="view_btn" class="btn btn-default" data-toggle="modal" data-target="#exampleModal">view</button><button id="update_btn" class="btn btn-default" data-toggle="modal" data-target="#exampleModal">update</button><button id="delete_btn" class="btn btn-default">delete</button><button id="down_btn" class="btn btn-default">down</button><button id="up_btn" class="btn btn-default">up</button></td>'
+                +'</tr>');
+
+        $('#name').val("")
+        $('#age').val("")
+    }
+
 
   });
 
@@ -68,6 +73,7 @@ $(document).ready(function(){
         i = $(this).closest('tr').find('td:eq(0)').text()
         $(this).closest('tr').remove();
         resetNumber()
+
   });
 
 //  Function to re-order serial number when row deleted.
@@ -75,6 +81,7 @@ $(document).ready(function(){
         i = $(this).closest('tr').find('td:eq(0)').text()
         $('#myTable').find('tr').find('td:first').each(function(i){
             $(this).text(parseInt(i)+1);
+            alert("Row delete successfully.");
         });
     }
 
@@ -113,6 +120,27 @@ $(document).ready(function(){
           $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+// Function for form validation.
+  function validateData(name, age) {
+    valid = true;
+    nameReg = /^[a-zA-Z]+$/;
+    ageReg = /^[0-9]+$/;
+    if (nameReg.test(name) == false) {
+        alert ("please enter valid string for name.");
+        valid = false;
+    }
+
+    if (valid != false && ageReg.test(age) == false) {
+        alert ("please enter valid number for age.");
+         valid = false;
+    }
+
+    return valid;
+  }
+
+
+
 
 });
 
